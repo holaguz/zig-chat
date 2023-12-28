34a5679608d4b94e6b2ac85237c6364ddd0769a8
holagus <agustin.catellani@gmail.com>
2023-12-28 02:34:01 -0300

    feat: clients can connect to the server via tcp
    the user can specify the port using the `--port` flag. defaults to 8080 if no port is specified.
    
    try using `nc localhost [port]` to connect to the server and a message will be printed to the server's stdout.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,84 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const AppParameters = struct {
+    port: u16,
+};
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+const Client = struct {
+    socket: std.os.socket_t,
+    addr: std.net.Address,
+};
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+var app_params = AppParameters{
+    .port = 8080,
+};
 
-    try bw.flush(); // don't forget to flush!
+var client_list = std.ArrayHashMap(std.os.socket_t, std.os.socket_t){};
+
+pub fn parseArgs(args: [][:0]u8) !void {
+    // TODO: Crash on unknown args, print usage, etc
+    const argc = args.len;
+
+    var i: usize = 1;
+    while (i < argc) : (i += 1) {
+        const arg = args[i];
+        if (std.mem.eql(u8, arg, "--port")) {
+            app_params.port = std.fmt.parseUnsigned(u16, args[i + 1], 10) catch |err| {
+                std.log.err("Couldn't parse port: {s}\n", .{args[i + 1]});
+                return err;
+            };
+            i += 1;
+        } else {
+            return error.InvalidArgument;
+        }
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn startServer() !std.os.socket_t {
+    const socket = try std.os.socket(std.os.AF.INET, std.os.SOCK.STREAM, 0);
+    const address = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, app_params.port);
+    const len = address.getOsSockLen();
+    try std.os.bind(socket, &address.any, len);
+    try std.os.listen(socket, 128);
+
+    return socket;
+}
+
+pub fn acceptClient(server_socket: std.os.socket_t) !Client {
+    var client_addr: std.net.Address = undefined;
+    var len: std.os.socklen_t = @sizeOf(std.os.sockaddr.in);
+    const client_socket = try std.os.accept(server_socket, &client_addr.any, &len, std.os.SOCK.NONBLOCK);
+
+    return Client{ .socket = client_socket, .addr = client_addr };
+}
+
+pub fn killClients() void {
+    // TODO
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    try parseArgs(args);
+
+    const serverSocket = try startServer();
+    defer std.os.close(serverSocket);
+
+    // If we don't kill the clients the server socket will linger on
+    defer killClients();
+
+    while (true) {
+        if (acceptClient(serverSocket)) |c| {
+            std.log.info("Accepted new client: {}\n", .{c.addr.in});
+        } else |err| {
+            std.log.err("Error accepting client: {any}\n", .{err});
+        }
+
+        std.time.sleep(1E9 * 0.1);
+    }
 }
